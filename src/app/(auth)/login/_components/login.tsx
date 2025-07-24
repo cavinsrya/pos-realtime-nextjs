78ff632b6a1ee94bb71b319ff9ac098737e9b7a4
cavinsrya <cavinsuryaa12@gmail.com>
2025-07-24 19:47:44 +0700

    feat(auth): add login page components, layout, and validation
    - Add login form components and layout
    - Implement Zod-based auth validation
    - Setup Supabase SQL for handling new user and delete user with triggers

---
+++ src/app/(auth)/login/_components/login.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { LoginForm, loginSchema } from "@/validations/auth-validation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { INITIAL_LOGIN_FORM } from "@/constants/auth-constants";
+import { Button } from "@/components/ui/button";
+import FormInput from "@/components/common/form-input";
+
+export default function Login() {
+  const form = useForm<LoginForm>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: INITIAL_LOGIN_FORM,
+  });
+
+  const onSubmit = form.handleSubmit(async (data) => {
+    console.log(data);
+  });
+
+  return (
+    <Card>
+      <CardHeader className="text-center">
+        <CardTitle className="text-xl">Welcome</CardTitle>
+        <CardDescription className="text-center">
+          Login to access all Features
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={onSubmit} className="space-y-4">
+            <FormInput
+              form={form}
+              name="email"
+              label="Email"
+              placeholder="Insert your email here"
+              type="email"
+            />
+            <FormInput
+              form={form}
+              name="password"
+              label="Password"
+              placeholder="******"
+              type="password"
+            />
+            <Button type="submit">Login</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+}
